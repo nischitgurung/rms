634acb5a6d1b15a353e52c4d872ff6b34d2b78f0
Nishchit Gurung <nischitgrg98@gmail.com>
2026-03-11 03:10:58 +0545

    Vercel Edit

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
